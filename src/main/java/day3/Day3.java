353a3f542fee32595d7e7f437148b5b9d319b465
Mael F <mael.fq44@gmail.com>
2024-12-08 18:59:02 +0100

    day 3 - part 2 - ok

--- src/main/java/day3/Day3.java
+++ src/main/java/day3/Day3.java
@@ -13,10 +13,9 @@ public class Day3 {
     public static void main(String[] args) {
 
         try {
-            System.out.println("Result: " + retrieveMultiplicationsSum());
+            //System.out.println("Result: " + retrieveMultiplicationsSum());
             System.out.println("Result with disabled parts: " + retrieveMultiplicationsSumWithDisabledParts());
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(Arrays.toString(e.getStackTrace()));
         }
     }
@@ -28,16 +27,16 @@ public class Day3 {
     }
 
     public static long retrieveMultiplicationsSumWithDisabledParts() throws IOException {
-        ArrayList<ArrayList<String>> input = parseCorruptedInputWithDisabledParts();
-        long sum = computeSumOfMultiplications(input);
+        ArrayList<String> input = parseCorruptedInputWithDisabledParts();
+        long sum = computeSumOfMultiplications1DArray(input);
         return sum;
     }
 
     public static long computeSumOfMultiplications(ArrayList<ArrayList<String>> input) {
         long sum = 0;
-        for(ArrayList<String> line : input) {
+        for (ArrayList<String> line : input) {
             long lineSum = 0;
-            for(String multiplication : line) {
+            for (String multiplication : line) {
                 long[] numbers = parseMultiplicationInput(multiplication);
                 long multiplicationResult = numbers[0] * numbers[1];
                 lineSum += multiplicationResult;
@@ -47,21 +46,31 @@ public class Day3 {
         return sum;
     }
 
+    public static long computeSumOfMultiplications1DArray(ArrayList<String> input) {
+        long sum = 0;
+        for (String multiplication : input) {
+            long[] numbers = parseMultiplicationInput(multiplication);
+            long multiplicationResult = numbers[0] * numbers[1];
+            sum += multiplicationResult;
+        }
+        return sum;
+    }
+
     public static long[] parseMultiplicationInput(String multiplication) {
-        long[] input = {0,0};
+        long[] input = {0, 0};
         Pattern pattern = Pattern.compile("[0-9]{1,3},[0-9]{1,3}", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(multiplication);
         boolean foundMatch = matcher.find();
-        if(foundMatch) {
+        if (foundMatch) {
             String group = matcher.group();
             String[] numbers = group.split(",");
 
-            if(numbers.length == 2) {
+            if (numbers.length == 2) {
                 input[0] = Long.parseLong(numbers[0]);
                 input[1] = Long.parseLong(numbers[1]);
             }
         }
-        
+
         return input;
     }
 
@@ -73,7 +82,7 @@ public class Day3 {
         String[] lines = data.split("\n");
 
         ArrayList<ArrayList<String>> corruptedInput = new ArrayList<>();
-        for(String line : lines) {
+        for (String line : lines) {
             Pattern pattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)", Pattern.CASE_INSENSITIVE);
             Matcher matcher = pattern.matcher(line);
 
@@ -87,39 +96,31 @@ public class Day3 {
         return corruptedInput;
     }
 
-    public static ArrayList<ArrayList<String>> parseCorruptedInputWithDisabledParts() throws IOException {
+    public static ArrayList<String> parseCorruptedInputWithDisabledParts() throws IOException {
         String fileName = "day3-input.txt";
         ClassLoader classLoader = Day3.class.getClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream(fileName);
         String data = readFromInputStream(inputStream);
-        String[] lines = data.split("\n");
 
-        ArrayList<ArrayList<String>> corruptedInput = new ArrayList<>();
-        for(String line : lines) {
-
-            ArrayList<String> enabledParts = getEnabledParts(line);
-
-            corruptedInput.add(enabledParts);
-        }
+        ArrayList<String> corruptedInput = getEnabledParts(data);
         return corruptedInput;
     }
 
-    private static ArrayList<String> getEnabledParts(String line) {
+    private static ArrayList<String> getEnabledParts(String input) {
         ArrayList<String> enabledParts = new ArrayList<>();
+
         Pattern pattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don't\\(\\)", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(line);
+        Matcher matcher = pattern.matcher(input);
 
         boolean isEnabled = true;
         while (matcher.find()) {
             String match = matcher.group();
             if (match.equals("do()")) {
                 isEnabled = true;
-            }
-            else if (match.equals("don't()")) {
+            } else if (match.equals("don't()")) {
                 isEnabled = false;
-            }
-            else {
-                if(isEnabled) {
+            } else {
+                if (isEnabled) {
                     enabledParts.add(match);
                 }
             }
